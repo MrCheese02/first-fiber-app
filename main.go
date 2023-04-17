8be62e3edd4c3a5e151f086cab4d05f27657598c
simon <simon.tilzer@outlook.com>
2023-04-17 13:30:34 +0200

    Adding static content hosting under /static.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "github.com/gofiber/fiber/v2"
 func main() {
     app := fiber.New()
 
+    app.Static("/static", "static/content")
+
     app.Get("/:neededVal/:optVal?/*", func(c *fiber.Ctx) error {
         return c.SendString("neededVal: " + c.Params("neededVal") + ", optVal: " + c.Params("optVal") + ", wildcard: " + c.Params("*"))
     })
