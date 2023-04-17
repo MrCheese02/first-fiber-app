91e32f463cf70d26ea5b3a8d212e916ba69d4513
simon <simon.tilzer@outlook.com>
2023-04-17 13:21:12 +0200

    Adding parameters to root.

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "github.com/gofiber/fiber/v2"
 func main() {
     app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
+    app.Get("/:neededVal/:optVal?/*", func(c *fiber.Ctx) error {
+        return c.SendString("neededVal: " + c.Params("neededVal") + ", optVal: " + c.Params("optVal") + ", wildcard: " + c.Params("*"))
     })
 
     app.Listen(":3000")
